19dddb0aef2108c11607ebcea92e16f965cd47cc
Nesokas <ines.moital@gmail.com>
2012-09-21 23:14:05 +0100

    Connection between server and client established (ping-pong)

--- smb.c
+++ smb.c
@@ -1,1 +1,56 @@
 //smb.c
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+int main(void) {
+	int fd, addrlen, ret, newfd;
+	int nread, nwritten, nbytes;
+	struct sockaddr_in addr;
+	char *ptr, buffer[128];
+	
+	if((fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+		exit(1);
+	
+	memset((void*)&addr, (int)'\0', sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	addr.sin_port = htons(59000);
+	
+	ret = bind(fd, (struct sockaddr*)&addr, sizeof(addr));
+	if(ret == -1)
+		exit(1);
+	
+	if(listen(fd,5) == -1)
+		exit(1);
+	
+	while(1) {
+		addrlen = sizeof(addr);
+		
+		if((newfd = accept(fd, (struct sockaddr*)&addr, &addrlen)) == -1)
+			exit(1);
+
+		nread = read(newfd, buffer, 128);
+		
+		if(nread == -1)
+			exit(1);
+		
+		puts(buffer);
+		
+		ptr = strcpy(buffer, "pong");
+		nbytes = 4;
+		
+		nwritten = write(newfd, ptr, nbytes);
+		
+		if(nwritten == -1)
+			exit(1);
+		
+		close(newfd);
+	}
+	
+	close(fd);
+	exit(0);
+}
--- user.c
+++ user.c
@@ -1,1 +1,44 @@
 //user.c
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <string.h>
+
+int main(void){
+	int fd, n, nbytes, nwritten, nread;
+	struct sockaddr_in addr;
+	struct hostent *hostptr;
+	char *ptr, buffer[128];
+	
+	fd = socket(AF_INET, SOCK_STREAM, 0);
+	if(fd == -1)
+		exit(1);
+		
+	hostptr = gethostbyname("localhost");
+		
+	memset((void*)&addr, (int)'\0', sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = ((struct in_addr*)(hostptr->h_addr_list[0]))->s_addr;
+	addr.sin_port = htons(59000);
+	
+	n = connect(fd,(struct sockaddr*)&addr, sizeof(addr));
+	if(n == -1)
+		exit(1);
+	
+	ptr = strcpy(buffer, "ping");
+	nbytes = 4;
+	
+	nwritten = write(fd, ptr, nbytes);
+	if(nwritten == -1)
+		exit(1);
+		
+	nread = read(fd, buffer, 128);
+	
+	puts(buffer);
+	
+	close(fd);
+	exit(0);
+}
